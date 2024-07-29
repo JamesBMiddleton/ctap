a2c9ae0dde95839b65b02ee8ad16e1f9e5e4cd86
James Middleton <james.ben.middleton@gmail.com>
2024-07-29 23:38:09 +0100

    Changes to macros.

--- test/example.c
+++ test/example.c
@@ -1,10 +1,12 @@
 #include "ctap.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 i32 main(void)
 {
     bool i = false;
-    ctp_logger = printf;
+    ctp_logger_cb = printf;
+    ctp_abort_cb = abort;
     ctp_load_map();
     LOG_W("i = %d\n", i);
     return 0;
