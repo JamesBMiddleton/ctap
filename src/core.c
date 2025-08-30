ee1cbc1034fa7d834137454ab1c9b46811ce0742
James Middleton <james.ben.middleton@gmail.com>
2025-08-30 22:14:21 +0100

    On the verge of the biggest restructure yet!

--- src/core.c
+++ src/core.c
@@ -18,20 +18,6 @@ typedef struct {
     uint numHorses;
 } EngineStarter;
 
-#define vec_TYPE EngineStarter
-#include "vec.c"
-
-// clang-format off
-// CPR = Const Pointer Result
-typedef struct { uint ok; core_Error err; } core_uintR;
-typedef struct { int ok; core_Error err; } core_intR;
-typedef struct { float ok; core_Error err; } core_floatR;
-typedef struct { const uchar* ok; core_Error err; } core_ucharCPR;
-typedef struct { uchar* ok; core_Error err; } core_ucharPR;
-typedef struct { const intVec* ok; core_Error err; } intVecCPR;
-typedef struct { const EngineStarterVec* ok; core_Error err; } EngineStarterVecCPR;
-// clang-format on
-
 typedef struct {
     uint placeholder;
 } core_InitArgs;
@@ -45,34 +31,24 @@ struct StateCore {
 } static gStateCore = {0};
 
 // placeholder comment
-static core_intR StartTheEngines(EngineStarter starter)
+static int StartTheEngines(EngineStarter starter)
 {
-    alloc_Arena scratch;
-    floatVec vec = floatVecInit(&scratch);
-    floatVecClear(&vec);
-
     if (starter.startiness != 0)
-        return (core_intR){.err = core_Error_MAP_INVALID};
+        return FAILURE
     util_LOGF_DEBUG("Engines started with %u startiness and %u horses!",
                     {.u = starter.startiness}, {.u = starter.numHorses});
-    return (core_intR){.ok = 1};
+    return SUCCESS;
 }
 
 // another placeholder comment
-static core_Error SpaghettifyValue(uint* value)
+static int SpaghettifyValue(uint* value)
 {
-    alloc_Arena scratch;
-    intVec vec = intVecInit(&scratch);
-    intVecPush(&vec, 1);
-    intVecPop(&vec);
-    intVecInsert(&vec, 0, 42);
-    intVecClear(&vec);
     *value = 0;
     return core_Error_MAP_INVALID;
 }
 
 // Initialise the core module.
-static core_Error core_Init(core_InitArgs args)
+static int core_Init(core_InitArgs args)
 {
     gStateCore.placeholder = args.placeholder;
     {
--- src/primitives.c
+++ src/primitives.c
@@ -2,21 +2,72 @@
 #define PRIMITIVES_C
 PRIMITIVES_C
 
+
+// ----------- freestanding compliant / header-only libc includes -------------
+
+#include <float.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
-typedef unsigned char uchar;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-typedef size_t usize;
-
+// ------------------ s2n-tls style result error handling pattern ------------
+#define CTP_SUCCESS 0
+#define CTP_FAILURE (-1)
 typedef struct {
-    const char* const chars;
-    const usize len;
-} str;
+    int retcode;
+} ctp_result;
+
+#if defined(__clang__) || defined(__GNUC__)
+    #define CTP_RESULT __attribute__((warn_unused_result)) ctp_result
+#else
+    #define CTP_RESULT ctp_result
+#endif
+// #define CTP_RESULT_OK
+// #define CTP_RESULT_ERROR
+// #define RESULT_ENSURE()
+// #define RESULT_ENSURE_PTR_VALID()
+// #define RESULT_BAIL()
+// ...
+
+
+// --------------- linux kernel 'safe' instrusive containers pattern ----------
+
+#if defined(__clang__) || defined(__GNUC__)
+#define container_of_const(ptr, type, member)                                  \
+    ((const type*)((const char*)(__typeof__(((const type*)0)->member)*){ptr} - \
+                   ((size_t) & (((type*)0)->member))))
+#else
+#define container_of_const(ptr, type, member) \
+    ((const type*)((const char*)(ptr) - ((size_t) & (((type*)0)->member))))
+#endif
+// struct ctp_list_head {};
+// ctp_list_init()
+// ctp_list_move()
+// ctp_list_swap()
+// ctp_list_for_each()
+// ...
+
+// ------------------ arena allocation pattern ------------------------
+
+// struct ctp_arena {};
+// ctp_arena_create()
+// ctp_arena_alloc()
+// ctp_arena_destroy()
+
+// ---------------- sokol inspired per module centralised data ownership -------
+
+// static struct { ctp_pool all_the_data; } ctp_audio_state;
+
+// ------------------ primitive types ---------------------------------
 
 // vec2
 // vec3
+
+// typedef struct {
+//     const char* const chars;
+//     const size_t len;
+// } str;
 
 // #define tostr(cstr) ((str) { .len = (sizeof("" cstr "")-1), .chars = (cstr) })
 //
